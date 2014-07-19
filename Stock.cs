4d0c5fd3f70bb43ffabeaff2d217aaceb547551a
codeguru <codeguru@users.sourceforge.net>
2014-07-18 21:32:05 -0600

    Refactor HTTP code to `doQuery()`

--- Stock.cs
+++ Stock.cs
@@ -15,17 +15,20 @@ namespace stock
 
 		public void getHistory (String startDate, String endDate)
 		{
+			Console.WriteLine(doQuery(startDate, endDate));
+		}
+
+		private String doQuery (String startDate, String endDate)
+		{
 			String yqlQuery = "select Date, Open, Close from yahoo.finance.historicaldata where symbol = \""
 				+ mSymbol + "\"  and startDate = \"" + startDate + "\" and endDate = \"" + endDate + "\"";
 			String url = "http://query.yahooapis.com/v1/public/yql?q=" + System.Uri.EscapeDataString(yqlQuery)
 				+ "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-			Console.WriteLine(url);
 
 			WebRequest request = WebRequest.Create(url);
 			WebResponse response = request.GetResponse();
 			StreamReader stream = new StreamReader(response.GetResponseStream());
-			Console.WriteLine(stream.ReadToEnd());
-			Console.WriteLine("done");
+			return stream.ReadToEnd();
 		}
 	}
 }
