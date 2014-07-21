f7756a960ed4c77b0a8201026d05591c5ed8725b
Code Guru <codeguru42@gmail.com>
2014-07-20 18:23:10 -0600

    Start to implement `NeuralNetwork`

--- NeuralNetwork.cs
+++ NeuralNetwork.cs
@@ -1,12 +1,39 @@
 using System;
+using System.Collections.Generic;
 
 namespace stock
 {
 	public class NeuralNetwork
 	{
-		public NeuralNetwork ()
+		private IList<IList<double>> inputEdges;
+		private IList<IList<double>> outputEdges;
+		private IList<IList<double>> hiddenEdges;
+
+		public void setInputEdges(IList<IList<double>> inputEdges)
 		{
+			this.inputEdges = deepCopy (inputEdges);
+		}
+
+		public void setOutputEdges(IList<IList<double>> outputEdges)
+		{
+			this.outputEdges = deepCopy (outputEdges);
+		}
+
+		public void setHiddenEdges(IList<IList<double>> hiddenEdges)
+		{
+			this.hiddenEdges = deepCopy (hiddenEdges);
+		}
+
+		public IList<double> evaluate(IList<double> inputs) {
+		}
+
+		private IList<IList<double>> deepCopy(IList<IList<double>> original)
+		{
+			IList<IList<double>> copy = new List<IList<double>> ();
+
+			foreach (IList<double> row in hiddenEdges) {
+				copy.Add (new List<double> (row));
+			}
 		}
 	}
 }
-
